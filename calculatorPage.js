3db39b275f0452bd8009519a9f1f93626e990286
Anastasiia <nastaparkhomchuk@gmail.com>
2024-05-11 17:03:21 +0300

    nothing

---
+++ calculatorPage.js
@@ -0,0 +1,38 @@
+class CalculatorPage {
+    get machineFamilyDropdown() { return $('[aria-labelledby="i18 i19"]'); }
+    get seriesDropdown() { return $('[aria-labelledby="i20 i21"]'); }
+    get machineTypeDropdown() { return $('[aria-labelledby="i22 i23"]'); }
+    get addGPUsButton() { return $('[aria-labelledby="i24 i25"]'); }
+    get gpuTypeDropdown() { return $('[aria-labelledby="i26 i27"]'); }
+    get numberOfGPUsDropdown() { return $('[aria-labelledby="i28 i29"]'); }
+    get localSSDInput() { return $('[aria-labelledby="i30"]'); }
+    get datacenterLocationDropdown() { return $('[aria-labelledby="i31 i32"]'); }
+    get committedUsageDropdown() { return $('[aria-labelledby="i33 i34"]'); }
+
+    async fillForm() {
+        await this.machineFamilyDropdown.click();
+        await $('[jsname="Fb0Bif"][aria-label="General Purpose"]').click();
+
+        await this.seriesDropdown.click();
+        await $('[jsname="Fb0Bif"][aria-label="N1"]').click();
+
+        await this.machineTypeDropdown.click();
+        await $('[jsname="Fb0Bif"][aria-label="n1-standard-8 (vCPUs: 8, RAM: 30 GB)"]').click();
+
+        await this.addGPUsButton.click();
+        await this.gpuTypeDropdown.click();
+        await $('[jsname="Fb0Bif"][aria-label="NVIDIA Tesla V100"]').click();
+        await this.numberOfGPUsDropdown.click();
+        await $('[jsname="Fb0Bif"][aria-label="1"]').click();
+
+        await this.localSSDInput.setValue('2x375 Gb');
+
+        await this.datacenterLocationDropdown.click();
+        await $('[jsname="Fb0Bif"][aria-label="Frankfurt (europe-west3)"]').click();
+
+        await this.committedUsageDropdown.click();
+        await $('[for="1-year"]').click();
+    }
+}
+
+module.exports = new CalculatorPage();
--- test/specs/test.spec.js
+++ test/specs/test.spec.js
@@ -22,12 +22,9 @@ describe('Search functionality', () => {
 
         const searchIcon = $('[jsname="Z5wyCf"]');
         searchIcon.click();
-
         const searchField = $('#i4');
         searchField.setValue('Google Cloud Platform Pricing Calculator');
-
         browser.keys('Enter');
-
         const searchResults = $('.search-results');
         // expect(searchResults).to.exist;
         expect(searchResults.isExisting());
@@ -36,16 +33,52 @@ describe('Search functionality', () => {
 
 
 
-
+// all what is down not working:
 
 
 it('should navigate to Pricing Calculator', async () => {
-    const pricingCalculatorLink = $('div.Nyd2Jd > div.x9K9hf.QKrqHf > a.K5hUy[href="https://cloud.google.com/pricing"]');
+    // const pricingCalculatorLink = $('div.Nyd2Jd');
+    const pricingCalculatorLink = $('c-wiz[@class="SSPGKf"]');
+
     expect(await pricingCalculatorLink.isExisting()).toBe(true);
     await pricingCalculatorLink.click();
-    await browser.waitUntil(async () => {
-        return (await browser.getUrl()) === 'https://cloud.google.com/pricing';
-    }, { timeout: 10000, timeoutMsg: 'Pricing Calculator page not loaded within 10 seconds' });
-    const currentUrl = await browser.getUrl();
-    expect(currentUrl).toBe('https://cloud.google.com/pricing');
+    // await browser.waitUntil(async () => {
+    //     return (await browser.getUrl()) === 'https://cloud.google.com/pricing';
+    // }, { timeout: 10000, timeoutMsg: 'Pricing Calculator page not loaded within 10 seconds' });
+    // const currentUrl = await browser.getUrl();
+    // expect(currentUrl).toBe('https://cloud.google.com/pricing');
 });
+
+it('should click on "Add to estimate"', async () => {
+    await $('[jsname="V67aGc"]').waitForDisplayed({ timeout: 10000 });
+    const addToEstimateButton = await $('[jsname="V67aGc"]');
+    await addToEstimateButton.click();
+    await browser.pause(2000);
+});
+it('should click on COMPUTE ENGINE', async () => {
+    await $('[jsname="m9ZlFb"]').waitForDisplayed({ timeout: 10000 });
+    const computeEngineButton = await $('[jsname="m9ZlFb"]');
+    await computeEngineButton.click();
+    await browser.pause(2000);
+});
+
+// it('should fill out the form with the number of instances', async () => {
+//     await $('input#c13').waitForDisplayed({ timeout: 10000 });
+//     const numberOfInstancesInput = await $('input#c13');
+//     await numberOfInstancesInput.setValue(4);
+//     await browser.pause(2000);
+
+
+//     const CalculatorPage = require('../calculatorPage');
+
+//     describe('Fill out the form on the Google Cloud calculator page', () => {
+//         it('should fill out the form with the specified data', async () => {
+//             await browser.url('https://cloud.google.com/products/calculator?hl=ru&dl=CiQyMGNjMzFjZi05NjVkLTQ3MzYtYjdiMC0zNTgwYTc3MmFmZTcQCBokMDA2QzIzRDQtNzZDRS00MzA0LUIxNDktOEQyODUxQ0E0RDAw');
+//             await CalculatorPage.fillForm();
+//         });
+//     });
+
+
+
+// });
+
